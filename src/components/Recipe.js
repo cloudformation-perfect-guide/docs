daa67f198b55342ed08dbad2a27ce25d2532c049
awaji-daisuke <awaji_daisuke@intec.co.jp>
2021-04-07 10:36:50 +0900

    add recipe

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./Recipe.css";
 import Link from "@docusaurus/Link";
 
-export const Recipe = ({ to, imageUrl, title }) => {
+export const Recipe = ({ to, imageUrl, title, subTitle }) => {
   return (
     <Link to={to}>
       <div className="recipe-container">
         <img className="recipe-architecture-image" src={imageUrl} />
         <div className="recipe-title">{title}</div>
+        <div className="recipe-sub-title">{subTitle}</div>
       </div>
     </Link>
   );
---
+++ src/components/Recipes.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Recipe } from "./Recipe";
+
+export const Recipes = () => {
+  const recipes = [
+    {
+      to: "/docs/cfn/recipe/apigateway_lambda_recipe",
+      imageUrl:
+        "https://serverlessland.com/assets/images/patterns/patterns-apigw-lambda-cdk.png",
+      title: "Amazon API Gateway to AWS Lambda",
+      subTitle: "Create an Amazon API Gateway to AWS Lambda REST API",
+    },
+    {
+      to: "/docs/cfn/recipe/apigateway_lambda_recipe",
+      imageUrl:
+        "https://serverlessland.com/assets/images/patterns/cdk-cloudfront-to-s3-and-lambda.png",
+      title: "CloudFront to S3 and API Gateway",
+      subTitle:
+        "CloudFront to S3 and API Gateway backed by TypeScript Lambda functions",
+    },
+  ];
+
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      {recipes.map(({ to, imageUrl, title, subTitle }) => {
+        return (
+          <div style={{ width: "50%", padding: "10px" }} key={title}>
+            <Recipe
+              to={to}
+              imageUrl={imageUrl}
+              title={title}
+              subTitle={subTitle}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
